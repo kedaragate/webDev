96a65dda0b1f80872d18059ebc07feedd9cedcbe
kedaragate <100003176+kedaragate@users.noreply.github.com>
2022-07-08 16:33:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -85,6 +85,9 @@ const modeButton=document.querySelector("#lightModeBtn");
 const switchMode=function (){
 modeButton.textContent="Dark Mode";
 document.body.style.backgroundColor="white";
+usersDiv.style.backgroundColor="white";
+card.style.backgroundColor="white";
+message.style.color="black";
 
 }
 modeButton.addEventListener("click", switchMode);
